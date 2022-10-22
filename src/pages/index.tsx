511b3a6edd787b6b1cfa0bdf90791b4f19ca78b9
Frédéric Massart <fred@branchup.tech>
2022-10-23 00:20:31 +0800

    Removing link to quest from the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,16 +9,13 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.hero)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/xp/docs">
             XP Docs
-          </Link>{' '}
-          <Link className="button button--secondary button--lg" to="/quest">
-            Quest
           </Link>
         </div>
       </div>
@@ -29,9 +26,8 @@ function HomepageHeader() {
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+    <Layout title={`${siteConfig.title}`}>
       <HomepageHeader />
-      <main></main>
     </Layout>
   );
 }
