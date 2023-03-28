931941d7c97a2a0e6f8231e1e82452c3963082f0
Frédéric Massart <fred@branchup.tech>
2023-03-28 17:07:11 +0800

    Adding Quests page to Getting Started

---
+++ src/components/Screenshot.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Screenshot.module.css';
+
+type Props = { img: { default: string } | string; children: React.ReactNode };
+
+export default function ({ img, children }: Props) {
+  const src = typeof img === 'string' ? img : img.default;
+  return (
+    <figure className={styles.figure}>
+      <img src={src} className={styles.img} />
+      <figcaption className={styles.figcaption}>{children}</figcaption>
+    </figure>
+  );
+}
