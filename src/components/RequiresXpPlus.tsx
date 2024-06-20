dc4805a59dc631c63b79998d807fdb0c8f5d9ea0
Frédéric Massart <fred@branchup.tech>
2024-06-20 10:50:20 +0800

    Adding some information about the level appearance

--- src/components/RequiresXpPlus.tsx
+++ src/components/RequiresXpPlus.tsx
@@ -1,11 +1,19 @@
 import Link from '@docusaurus/Link';
 import styles from './RequiresXpPlus.module.css';
 
-export default function () {
+export default function ({ someOnly = false }: { someOnly: boolean }) {
   return (
     <p className={styles.content}>
       <div className={styles.pill}>
-        This features requires <Link to="/xp/docs/#xp-plus">Level Up XP+</Link>
+        {!someOnly ? (
+          <>
+            This features requires <Link to="/xp/docs/#xp-plus">Level Up XP+</Link>
+          </>
+        ) : (
+          <>
+            Some features require <Link to="/xp/docs/#xp-plus">Level Up XP+</Link>
+          </>
+        )}
       </div>
     </p>
   );
