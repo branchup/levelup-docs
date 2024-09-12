0ba08465e05fa12b7aa4ffd75837b7037dcc5778
Frédéric Massart <fred@branchup.tech>
2024-09-13 01:28:20 +0800

    Document the leaderboard isolation options

--- src/components/RequiresXpPlus.tsx
+++ src/components/RequiresXpPlus.tsx
@@ -1,17 +1,17 @@
 import Link from '@docusaurus/Link';
 import styles from './RequiresXpPlus.module.css';
 
-export default function ({ someOnly = false }: { someOnly: boolean }) {
+export default function ({ someOnly = false, plan = '' }: { someOnly: boolean; plan?: string }) {
   return (
     <p className={styles.content}>
       <div className={styles.pill}>
         {!someOnly ? (
           <>
-            This features requires <Link to="/xp/docs/#xp-plus">Level Up XP+</Link>
+            This features requires <Link to="/xp/docs/#xp-plus">Level Up XP+ {plan}</Link>
           </>
         ) : (
           <>
-            Some features require <Link to="/xp/docs/#xp-plus">Level Up XP+</Link>
+            Some features require <Link to="/xp/docs/#xp-plus">Level Up XP+ {plan}</Link>
           </>
         )}
       </div>
