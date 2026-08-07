99ac1d57306330b2fd5e9516fa8540e35ecdbd82
Frédéric Massart <fred@branchup.tech>
2026-08-07 22:15:18 +0800

    Revisit the index page for XP

--- src/components/RequiresXpPlus.tsx
+++ src/components/RequiresXpPlus.tsx
@@ -1,20 +1,15 @@
 import Link from '@docusaurus/Link';
 import styles from './RequiresXpPlus.module.css';
 
-export default function ({ someOnly = false, plan = '' }: { someOnly: boolean; plan?: string }) {
+export default function ({ someOnly = false, plan = '' }: { someOnly?: boolean; plan?: string }) {
+  const name = `Level Up XP+${plan ? ` ${plan}` : ''}`;
+
   return (
     <p className={styles.content}>
-      <div className={styles.pill}>
-        {!someOnly ? (
-          <>
-            This features requires <Link to="/xp/docs/#xp-plus">Level Up XP+ {plan}</Link>
-          </>
-        ) : (
-          <>
-            Some features require <Link to="/xp/docs/#xp-plus">Level Up XP+ {plan}</Link>
-          </>
-        )}
-      </div>
+      <span className={styles.pill}>
+        {someOnly ? 'Some features require ' : 'This feature requires '}
+        <Link to="/xp/docs#xp-plus">{name}</Link>.
+      </span>
     </p>
   );
 }
